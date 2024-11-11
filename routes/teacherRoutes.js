8fb52cdb9be1ddf72ebff550176d6a039ac118ec
BadaHong <badah@hanyang.ac.kr>
2024-11-11 19:11:22 +0900

    feat: teacher signup with diff method

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -8,6 +8,9 @@ const authenticateJWT = require('../jwt/auth');
 router.get('/', teacherController.getTeachers);
 //router.get('/', authenticateJWT, teacherController.getTeachers);
 router.post('/signup', upload.single('image'),teacherController.signupTeacher);
+router.post('/signup/kakao', upload.single('image'),teacherController.signupTeacherKakao);
+router.post('/signup/google', upload.single('image'),teacherController.signupTeacherGoogle);
+router.post('/signup/mail', upload.single('image'),teacherController.signupTeacherMail);
 router.post('/login', teacherController.loginTeacher);
 
 module.exports = router;
