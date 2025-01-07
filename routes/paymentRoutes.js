12e38f3c5ac35562740fbbcc06d4e44caf3c5cf7
BadaHong <badah@hanyang.ac.kr>
2025-01-07 13:09:02 +0900

    fix: error in routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,10 +5,10 @@ const upload = multer({ storage: multer.memoryStorage() });
 const paymentController = require('../controllers/paymentController');
 const authenticateJWT = require('../jwt/auth');
 
-router.get('/unpaidS', authenticateJWT, (req, res) => {
+router.get('/unpaidT', authenticateJWT, (req, res) => {
     paymentController.getUnpaidDatesTeacher(req, res)
 });
-router.get('/unpaidT', authenticateJWT, (req, res) => {
+router.get('/unpaidS', authenticateJWT, (req, res) => {
     paymentController.getUnpaidDatesStudent(req, res)
 });
 
